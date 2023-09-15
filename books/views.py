19c9221bcfbe6ea53018a9f9b7ba6d92ae47d496
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-15 16:44:57 +0300

    add endpoint books/book_id/

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from books.models import Book
 
@@ -6,3 +6,12 @@ from books.models import Book
 def all_books_view(request: HttpRequest) -> HttpResponse:
     books = Book.objects.all()
     return render(request, 'all_books.html', context={'books': books})
+
+
+def book_view(request: HttpRequest, book_id: int) -> HttpResponse | HttpResponseNotFound:
+    try:
+        book = Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        return HttpResponseNotFound()
+
+    return render(request, 'book.html', context={'book': book})
--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from books.views import all_books_view
+from books.views import all_books_view, book_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', all_books_view)
+    path('books/', all_books_view),
+    path('books/<int:book_id>/', book_view)
 ]
