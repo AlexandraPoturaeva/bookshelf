c1279e1b701dba3e8f59a95b36090f8f902794a7
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-15 15:55:34 +0300

    add typing to endpoint /books/

--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from books.models import Book
 
 
-def all_books_view(request):
+def all_books_view(request: HttpRequest) -> HttpResponse:
     books = Book.objects.all()
     return render(request, 'all_books.html', context={'books': books})
