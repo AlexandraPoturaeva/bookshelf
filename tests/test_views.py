f8e8c53e759e9c900aa3f925c4af43229b984415
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-19 16:42:17 +0300

    follow the rules of tests naming

--- tests/test_views.py
+++ tests/test_views.py
@@ -15,31 +15,31 @@ class BookViewTest(TestCase):
                 year_of_publishing=2000 + book_id,
                 copies_printed=book_id * 10,
                 short_description=f'Описание книги {book_id}',
-                )
+            )
 
-    def test_all_books_view_url_exists_at_desired_location(self):
+    def test__all_books_view__url_exists_at_desired_location(self):
         response = self.client.get('/books/')
         self.assertEqual(response.status_code, 200)
 
-    def test_all_books_view_uses_correct_template(self):
+    def test__all_books_view__uses_correct_template(self):
         response = self.client.get(reverse('all_books'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'all_books.html')
 
-    def test_one_book_url_exists_at_desired_location_by_id(self):
+    def test__book_view__url_exists_at_desired_location_by_id(self):
         response = self.client.get('/books/3/')
         self.assertEqual(response.status_code, 200)
 
-    def test_one_book_view_not_found(self):
+    def test__book_view__not_found_if_id_doesnt_exist(self):
         response = self.client.get('/books/7/')
         self.assertEqual(response.status_code, 404)
 
-    def test_one_book_view_uses_correct_template(self):
+    def test__book_view__uses_correct_template(self):
         response = self.client.get(reverse('one_book', kwargs={'book_id': 3}))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'book.html')
 
-    def test_one_book_details_handler_return_correct_json(self):
+    def test__one_book_details_handler__returns_correct_json(self):
         book_id = 2
         expected_json = {
             "id": book_id,
