c4fed61dd83aa6ca9fa7015d15aeeecee8b4a44d
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-19 16:42:50 +0300

    move helping functions from views to models

--- books/models.py
+++ books/models.py
@@ -10,3 +10,20 @@ class Book(models.Model):
 
     def __str__(self) -> str:
         return f"{self.title} ({self.author_full_name})"
+
+    def get_book_details(self) -> dict[str, int | str]:
+        return {
+            "id": self.pk,
+            "title": self.title,
+            "author_full_name": self.author_full_name,
+            "year_of_publishing": self.year_of_publishing,
+            "copies_printed": self.copies_printed,
+            "short_description": self.short_description,
+        }
+
+
+def get_book_by_id(book_id: int) -> Book | None:
+    try:
+        return Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        return None
